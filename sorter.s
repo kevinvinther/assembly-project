3002f9a1ee89d906435393743f6993121298f623
kevinvinther <kevinjoshua.kjv@gmail.com>
2021-10-30 15:01:06 +0200

    Add almost-working counting sort

--- sorter.s
+++ sorter.s
@@ -21,6 +21,10 @@
 		.space 8
 	lineCount: 
 		.space 8
+	countingBuffer:
+		.space 8
+	sortedBuffer:
+		.space 8
 .section .text
 .globl _start
 
@@ -96,59 +100,111 @@ _start:
 	
 # TODO: Remove when donem
 # TODO: take shower
-insertSort: 
-	########################
-	# Begin Insertion Sort #
-	########################
+countingSort: 
+	#########################
+	# Begin Counting Sort 	#
+	#########################
 
 	movq lineCount, %rax	# put linecount in rax register
 	cmpq $1, %rax			# if the linecount is <1
 	jle printLoop			# exit, the list is already sorted
 
 
-	movq $2, %r13					# r13 = 2, skip first line
-	imul $2, lineCount, %rax		# rax = 2 * lineCount
-
 	movq parsedBuffer, %r12		# r12 = parsedBuffer 
+
+	# We know that the maximum value of a coordinate (x or y) will be 32767, with 0 that is 32768
+	# A number is 8 bytes, therefore we multiply it with 8
+	movq $32768, %rdi
+	imulq $8, %rdi, %rdi
+	call allocate
+	movq %rax, countingBuffer
+	# put allocated space in countingBuffer
+
+	movq $0, %rdi
+	movq countingBuffer, %r10
+# Set countingbuffer = 0
+setZeroLoop:
+	# use rdi as index
+	movq $0, (%r10, %rdi)
+	addq $1, %rdi	
+	cmpq $32768, %rdi
+	jl setZeroLoop
+
+
+	# Start at 1, because we only want y-coordinates
+	movq $1, %rdi
+	movq parsedBuffer, %r11
+	# parsedBuffer length = linecount*2	
+	imulq $2, lineCount, %rax
+
+storeNumCounter: # the counting array 
+	# Put value of a number into r12
+	movq (%r11, %rdi, 8), %r12 # Value of parsedBuffer[rdi]
+
+	# Take r12 index of zero-array, i.e. zeroArray[r12*8] += 1
+	addq $1, (%r10, %r12, 8)
 	
-outerLoop:
-	cmpq %rax, %r13		# If the current line == number of lines
-	je printLoopBegin	# Then print
+	# add 2 because we only want y-coordinates
+	addq $2, %rdi
+	cmpq %rdi, %rax
+	jg storeNumCounter
 
-	movq %r13, %rdi		# rdi = r13, temporary counter
+	# Start at index 1 
+	movq $1, %rdi
+getPositions: 
+	movq -8(%r10, %rdi, 8), %r12	# counter[rdi-1]
+	addq %r12, (%r10, %rdi, 8)
+
+	addq $1, %rdi	
+	cmpq $32768, %rdi
+	jl getPositions
+
+	movq %r10, countingBuffer
+
+startSortBuffer:
+	# Start at index 0
+	# Allocate space for our sorted buffer
+	movq lineCount, %rdi
+	imulq $16, %rdi	# 2 numbers, each 8 bytes, 
+	call allocate
+	movq %rax, sortedBuffer
 	
-innerLoop:
-	cmpq $0, %rdi
-	je outerLoop
-
-	movq %rdi, %r10		# rbp = rdi, temporary counter 2  
-	subq $2, %r10		# rbp -= 2 
-
-	movq 8(%r12, %r10, 8), %r14	# y_1
-	cmpq 8(%r12, %rdi, 8), %r14	# if y[rdi] < y[rbp]
-	jl outerLoopEnd				# 	then jump to outerLoopEnd
 	
-	movq 8(%r12, %rdi, 8), %r15	#	the second y-element
-	movq (%r12, %rdi, 8), %r8	# the second x-element
-	movq (%r12, %r10, 8), %r9	# the first x-element
 
-	# swap the x values
-	movq %r9, (%r12, %rdi, 8)
-	movq %r8, (%r12, %r10, 8)
-	# swap the y values
-	movq %r15, 8(%r12, %r10, 8)
-	movq %r14, 8(%r12, %rdi, 8)
+	movq $1, %rdi # Counter
 
-	subq $2, %rdi		# sub from decreasing counter
-	jmp innerLoop
+	movq lineCount, %r8
+	imulq $2, %r8
+	movq parsedBuffer, %r11
+	movq sortedBuffer, %r14
 
-outerLoopEnd:
-	addq $2, %r13
-	jmp outerLoop
+	movq countingBuffer, %r10
+sortBuffer:
+	movq (%r11, %rdi, 8), %r9
+
+	movq (%r10, %r9, 8), %r13
+
+	imulq $2, %r13 # r12 = r12*2
+
+	movq %r9, 8(%r14, %r13, 8)		# y-coord in place
+
+	
+
+	subq $1, (%r10, %r9, 8)
+
+	movq -8(%r11, %rdi, 8), %r9
+	movq %r9, (%r14, %r13, 8)	# place x-val for y-coord
+
+	addq $2, %rdi
+	cmpq %rdi, %r8
+	jg sortBuffer
+
+	movq sortedBuffer, %r10
+
 printLoopBegin:
 	xor %r13, %r13				# r13 = 0
 	imul $2, lineCount, %rax
-	movq parsedBuffer, %r12		# r12 = parsedBuffer
+	movq %r10, %r12		# r12 = parsedBuffer	
 printLoop:
 	movq (%r12, %r13, 8), %r10	# r10 = r12[r13*8] 
 	movq %r10, %rdi				# rdi = r10
